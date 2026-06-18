f742f74db1a59ae4fec53ca115213f96f93a1f1b
KylerNexTech <Kylersmith1101@gmail.com>
2026-06-18 13:05:00 -0400

    Version 2: Blend Mode Difference

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-let colors = {
-    red,
-    blue,
-    green,
-    purple,
+const cursor = document.querySelector('.div2');
+
+let mouseX = 0;
+let mouseY = 0;
+let x = 0;
+let y = 0;
+
+document.addEventListener('mousemove', (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+});
+
+function animate() {
+    x += (mouseX - x) * 0.15;
+    y += (mouseY - y) * 0.15;
+
+    cursor.style.transform =
+        `translate(${x - 40}px, ${y - 40}px)`;
+
+    requestAnimationFrame(animate);
 }
+
+animate();
